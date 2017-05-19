7222dea005d8799c6aa00ac4f166642839fa9581
CuityMiko <15201776956@163.com>
2017-05-20 07:06:20 +0800

    完成分页

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@
 	]);
 	//配置路由规则
 	moviecatApp.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.otherwise({redirectTo: '/in_theaters'}); //当没有匹配到路由的时候则会跳转到默认配置的路由上去
+		$routeProvider.otherwise({redirectTo: '/in_theaters/1'}); //当没有匹配到路由的时候则会跳转到默认配置的路由上去
 	}]);
 
 })(angular)
--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -6,7 +6,7 @@
 	var in_theaters=angular.module('moviecatApp.in_theaters', ['ngRoute','moviecatApp.services.http']);
 	//配置路由
 	in_theaters.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/in_theaters', {
+		$routeProvider.when('/in_theaters/:page', {
 			templateUrl: 'in_theaters/index.html',
 			controller: 'IntheatersController'
 		});
@@ -16,11 +16,16 @@
 	//此时jsonp中的url就必须加上callback=JSON_CALLBACK
 	//由于默认的angular提供的异步请求对象不支持自定义回调函数名，angular随机分配的回调函数名称不被第三方API支持
 	//则需要自定义一个跨域的服务在angular的controller中注入进来：HttpService
-	in_theaters.controller('IntheatersController', ['$scope','$http','HttpService',function($scope,$http,HttpService) {
+	in_theaters.controller('IntheatersController', ['$scope','$http','$route','$routeParams','HttpService',function($scope,$http,$route,$routeParams,HttpService) {
+		var page=parseInt($routeParams.page); //获取当前页
+		var pagecount=10; //每页显示条数
+		var start=(page-1)*pagecount;
 		$scope.movies={};
 		$scope.massage='';
 		$scope.total=0;
 		$scope.loading=true;
+		$scope.currentpage=page;
+		$scope.pagetotal=0;
 		//$http,jsonp方式已经无法使用
 		// $http.jsonp('http://api.douban.com/v2/movie/in_theaters?callback=JSONP_CALLBACK&city=上海&start=1&count=10').then((result)=>{
 		// 	if(result.status==200)
@@ -32,14 +37,15 @@
 		// });
 		//自定义HttpService
 		HttpService.jsonp('http://api.douban.com/v2/movie/in_theaters',{
-			// start:1,
-			// count:10,
+			start:start,
+			count:pagecount,
 			city:'上海'
 		},function(result){
 			if(result){
 				$scope.movies=result;
 				$scope.total=result.total;
 				$scope.loading=false;
+				$scope.pagetotal=Math.ceil(result.total/pagecount);
 				//由于在调用第三方API的时候$scope的数据已经在页面上加载完毕，则需要调用$scope.$apply()方法$scope的数据再重新同步一下
 				$scope.$apply();
 			}
@@ -48,5 +54,11 @@
 				$scope.massage='请求数据失败';
 			}
 		})
+		//点击翻页行为
+		$scope.go=function(page){
+			if(page>=1 && page<=$scope.pagetotal){
+				$route.updateParams({page:page});
+			}
+		}
 	}]);
 })(angular)
